7e17152347e40a78bcfb7c63ad2239fad4c225fa
Rohith Kasar <mooted@rohith-kasar.local>
2019-07-12 13:13:51 -0700

    Initial Commit

---
+++ StockTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockTracker
+//
+//  Created by Rohith Kasar on 7/12/19.
+//  Copyright © 2019 rohith.kasar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StockTrackerTests/StockTrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  StockTrackerTests.swift
+//  StockTrackerTests
+//
+//  Created by Rohith Kasar on 7/12/19.
+//  Copyright © 2019 rohith.kasar. All rights reserved.
+//
+
+import XCTest
+@testable import StockTracker
+
+class StockTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
